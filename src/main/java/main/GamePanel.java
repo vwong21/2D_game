857e781bab6525aec7479cb6bdcbb0544bd6c836
Vincent Wong <vincentwong5609@gmail.com>
2025-04-08 14:51:20 -0700

    loaded map

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -17,10 +17,10 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // FPS
     int FPS = 60;
@@ -88,7 +88,7 @@ public class GamePanel extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D)g;
 
         tileM.draw(g2);
-        
+
         player.draw(g2);
 
         g2.dispose();

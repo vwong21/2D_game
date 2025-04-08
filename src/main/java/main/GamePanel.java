94c8fb1b34f40cfdca80c4f210204b7251b473d1
Vincent Wong <vincentwong5609@gmail.com>
2025-04-08 14:30:29 -0700

    added tile

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
     
@@ -24,14 +25,10 @@ public class GamePanel extends JPanel implements Runnable {
     // FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this,keyH);
-
-    // Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
 
@@ -90,6 +87,8 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D)g;
 
+        tileM.draw(g2);
+        
         player.draw(g2);
 
         g2.dispose();
